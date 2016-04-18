9930dbaf5781745f4afb5346e5d406547a480493
Deep <deep.arora@oncam.com>
2016-04-18 20:23:44 +0530

    Initial Commit

---
+++ OncamSDKTest/OncamSDKTest.h
@@ -0,0 +1,13 @@
+//
+//  OncamSDKTest.h
+//  OncamSDKTest
+//
+//  Created by Venkat Rajeev on 18/04/16.
+//  Copyright © 2016 OnCam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OncamSDKTest : NSObject
+
+@end
---
+++ OncamSDKTest/OncamSDKTest.m
@@ -0,0 +1,13 @@
+//
+//  OncamSDKTest.m
+//  OncamSDKTest
+//
+//  Created by Venkat Rajeev on 18/04/16.
+//  Copyright © 2016 OnCam. All rights reserved.
+//
+
+#import "OncamSDKTest.h"
+
+@implementation OncamSDKTest
+
+@end
